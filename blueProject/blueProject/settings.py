7029aeab449cbfefcc5871e2aaa981ca9c894ffe
tmfrlrkvlek <wltn102998@gmail.com>
2021-06-27 04:00:54 +0900

    0627 do collectstatic

--- blueProject/blueProject/settings.py
+++ blueProject/blueProject/settings.py
@@ -124,6 +124,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'blog', 'static'),
 ]
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 # Default primary key field type
